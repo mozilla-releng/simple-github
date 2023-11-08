c3498f91dc926ddf148cbf3f8e54b82fd0717750
Andrew Halberstadt <ahal@mozilla.com>
2023-11-08 12:10:14 -0500

    feat: add support for synchronous environments

--- src/simple_github/client.py
+++ src/simple_github/client.py
@@ -1,11 +1,16 @@
+import asyncio
 import json
+from abc import abstractmethod
 from typing import TYPE_CHECKING, Any, Dict, List, Optional, Union
 
 from aiohttp import ClientSession, ContentTypeError
 from gql import Client as GqlClient
 from gql import gql
-from gql.client import ReconnectingAsyncClientSession
+from gql.client import ReconnectingAsyncClientSession, SyncClientSession
 from gql.transport.aiohttp import AIOHTTPTransport
+from gql.transport.requests import RequestsHTTPTransport
+from requests import Session
+from requests.exceptions import JSONDecodeError
 
 if TYPE_CHECKING:
     from simple_github.auth import Auth
@@ -30,9 +35,189 @@ class Client:
         """
         self.auth = auth
         self._prev_token = None
-        self._gql_client = None
-        self._gql_session = None
+        self._gql_client: Optional[GqlClient] = None
+        self._gql_session: Optional[
+            Any[ReconnectingAsyncClientSession, SyncClientSession]
+        ] = None
 
+    @abstractmethod
+    def close(self) -> None:
+        ...
+
+    @abstractmethod
+    def request(self, method: str, query: str, **kwargs: Any) -> Response:
+        ...
+
+    @abstractmethod
+    def get(self, query: str) -> Response:
+        ...
+
+    @abstractmethod
+    def post(self, query: str, data: RequestData = None) -> Response:
+        ...
+
+    @abstractmethod
+    def put(self, query: str, data: RequestData = None) -> Response:
+        ...
+
+    @abstractmethod
+    def patch(self, query: str, data: RequestData = None) -> Response:
+        ...
+
+    @abstractmethod
+    def delete(self, query: str, data: RequestData = None) -> None:
+        ...
+
+    @abstractmethod
+    def execute(self, query: str, variables: RequestData = None) -> Dict[str, Any]:
+        ...
+
+
+class SyncClient(Client):
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *excinfo: Any):
+        self.close()
+
+    def close(self) -> None:
+        asyncio.run(self.auth.close())
+        if self._gql_client:
+            self._gql_client.close_sync()
+
+    def _get_gql_session(self) -> SyncClientSession:
+        """Return an AIOHTTP session.
+
+        The session will be automatically re-created anytime the auth's
+        token changes.
+
+        Returns:
+            aiohttp.ClientSession: An AIOHTTP session object.
+        """
+        token = asyncio.run(self.auth.get_token())
+
+        if token == self._prev_token:
+            assert isinstance(self._gql_session, SyncClientSession)
+            return self._gql_session
+
+        # Create a new session with updated token.
+        self._prev_token = token
+        if self._gql_client:
+            self._gql_client.close_sync()
+
+        headers = {
+            "Accept": "application/vnd.github+json",
+            "Authorization": f"Bearer {token}",
+        }
+        transport = RequestsHTTPTransport(url=GITHUB_GRAPHQL_ENDPOINT, headers=headers)
+        self._gql_client = GqlClient(
+            transport=transport, fetch_schema_from_transport=False
+        )
+        self._gql_session = self._gql_client.connect_sync()
+        assert isinstance(self._gql_session, SyncClientSession)
+        return self._gql_session
+
+    def _get_requests_session(self) -> Session:
+        session = self._get_gql_session()
+        assert isinstance(session.transport, RequestsHTTPTransport)
+        assert session.transport.session
+        return session.transport.session
+
+    def request(self, method: str, query: str, **kwargs) -> Response:
+        """Make a request to Github's REST API.
+
+        Args:
+            method (str): The HTTP method, either 'GET' or 'POST'.
+            query (str): The path segment of the request, e.g `/octocat`.
+            kwargs (Dict): Extra args to pass to
+                `aiohttp.ClientSession.request`.
+
+        Returns:
+            Dict: The JSON result of the request.
+        """
+        url = f"{GITHUB_API_ENDPOINT}/{query.lstrip('/')}"
+        session = self._get_requests_session()
+
+        with session.request(method, url, **kwargs) as resp:
+            if not resp.ok:
+                resp.raise_for_status()
+            try:
+                return resp.json()
+            except JSONDecodeError:
+                return resp.text.strip('"')
+
+    def get(self, query: str) -> Response:
+        """Make a GET request to Github's REST API.
+
+        Args:
+            query (str): The path segment of the request, e.g `/octocat`.
+
+        Returns:
+            Dict: The JSON result of the request.
+        """
+        return self.request("GET", query)
+
+    def post(self, query: str, data: RequestData = None) -> Response:
+        """Make a POST request to Github's REST API.
+
+        Args:
+            query (str): The path segment of the request, e.g `/octocat`.
+            data (Dict): The data to send in the request (optional).
+
+        Returns:
+            Dict: The JSON result of the request.
+        """
+        return self.request("POST", query, data=json.dumps(data))
+
+    def put(self, query: str, data: RequestData = None) -> Response:
+        """Make a PUT request to Github's REST API.
+
+        Args:
+            query (str): The path segment of the request, e.g `/octocat`.
+            data (Dict): The data to send in the request (optional).
+
+        Returns:
+            Dict: The JSON result of the request.
+        """
+        return self.request("PUT", query, data=json.dumps(data))
+
+    def patch(self, query: str, data: RequestData = None) -> Response:
+        """Make a PATCH request to Github's REST API.
+
+        Args:
+            query (str): The path segment of the request, e.g `/octocat`.
+            data (Dict): The data to send in the request (optional).
+
+        Returns:
+            Dict: The JSON result of the request.
+        """
+        return self.request("PATCH", query, data=json.dumps(data))
+
+    def delete(self, query: str, data: RequestData = None) -> None:
+        """Make a DELETE request to Github's REST API.
+
+        Args:
+            query (str): The path segment of the request, e.g `/octocat`.
+            data (Dict): The data to send in the request (optional).
+        """
+        self.request("DELETE", query, data=json.dumps(data))
+
+    def execute(self, query: str, variables: RequestData = None) -> Dict[str, Any]:
+        """Execute a query against Github's GraphQL endpoint.
+
+        Args:
+            query (str): The GraphQL query to execute.
+            variables (Dict): The GraphQL variables associated with the query
+                (optional).
+
+        Returns:
+            Dict: The result of the executed query.
+        """
+        session = self._get_gql_session()
+        return session.execute(gql(query), variable_values=variables)
+
+
+class AsyncClient(Client):
     async def __aenter__(self):
         return self
 
